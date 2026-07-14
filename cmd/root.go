cf10972fe7ccd4fba8f4fa49d059ea8540986dd8
Samuel K <skevetter@pm.me>
2026-07-14 08:51:29 -0500

    Revert "fix: support DEVPOD_LOG_LEVEL and DEVPOD_DEBUG environment variables …" (#270)
    This reverts commit b0dfd642c4b47804e20005f31b785291a96b8bd7.

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/sirupsen/logrus"
 	"github.com/skevetter/log"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/ssh"
@@ -19,13 +18,6 @@ func NewRootCmd() *cobra.Command {
 		SilenceUsage:  true,
 
 		PersistentPreRunE: func(cobraCmd *cobra.Command, args []string) error {
-			if lvl, err := logrus.ParseLevel(os.Getenv("DEVPOD_LOG_LEVEL")); err == nil {
-				log.Default.SetLevel(lvl)
-			}
-			if os.Getenv("DEVPOD_DEBUG") == "true" {
-				log.Default.SetLevel(logrus.DebugLevel)
-			}
-
 			log.Default.MakeRaw()
 
 			return nil
